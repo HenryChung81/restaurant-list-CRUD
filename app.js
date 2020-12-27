3117d3fdf4b805a9f83f4c6b2da0f23fc9bbaa81
Henry Chung <chungjenhao81@gmail.com>
2020-12-27 10:54:41 +0800

    fix: handlebars

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const exphbs = require("express-handlebars");
 const restaurantList = require("./restaurant.json");
 
 // setting template engine
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 // setting static files
 app.use(express.static("public"));
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 // routes setting
 app.get("/", (req, res) => {
   // past the restaurant data into 'index' partial template
-  res.render("index", { restaurants: restaurantList.results });
+  res.render("index");
 });
 
 app.get("/restaurants/:restaurant_id", (req, res) => {
