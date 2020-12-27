2a66370d32c783910743c12705d1c52c682be36e
Henry Chung <chungjenhao81@gmail.com>
2020-12-27 11:37:54 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // require package used in the project
 const express = require("express");
+const exphbs = require("express-handlebars");
+const RestaurantListModels = require("./models/restaurant")
 const mongoose = require("mongoose")
 const app = express();
 const port = 3000;
 
+// setting template engine
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+// setting static files
+app.use(express.static("public"));
+
+// connection
 mongoose.connect("mongodb://localhost/restaurant-list", { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
@@ -16,21 +26,13 @@ db.once("open", () => {
   console.log("mongodb connected!")
 })
 
-// require express-handlebars and restaurantList here
-const exphbs = require("express-handlebars");
-const restaurantList = require("./restaurant.json");
-
-// setting template engine
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
-app.set("view engine", "hbs");
-
-// setting static files
-app.use(express.static("public"));
-
 // routes setting
 app.get("/", (req, res) => {
   // past the restaurant data into 'index' partial template
-  res.render("index");
+  RestaurantListModels.find() //取出 RestaurantListModels 裡面的所有資料
+    .lean() //把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(restaurants => res.render("index", { restaurants })) //將資料傳給 index 樣板
+    .catch(error => console.log(error)) //錯誤處理
 });
 
 app.get("/restaurants/:restaurant_id", (req, res) => {
